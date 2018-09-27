a56fa0a7528792dc4dde021b9e0380c0d4e5ee04
Px Mx <foospidy@users.noreply.github.com>
2018-09-27 18:11:18 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -18,8 +18,6 @@ import os
 sys.path.append(os.path.abspath('..'))
 sys.path.append(os.path.abspath('./demo/'))
 
-from sphinx_rtd_theme import __version__
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
